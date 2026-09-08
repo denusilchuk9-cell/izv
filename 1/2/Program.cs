f806e2e1c9b58e729940d9a3a1095b822a26d0c9
𝓑𝓵𝓪𝓷𝓴 <denusilchuk9@gmail.com>
2026-09-08 15:29:52 +0300

    Add files via upload

---
+++ 1/2/Program.cs
@@ -0,0 +1,11 @@
+namespace LoanCalculatorApp;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
